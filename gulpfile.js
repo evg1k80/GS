7a1cb780b63be55a3db838a14a2f8c1bda3690ef
evg1k80 <evg1k80@gmail.com>
2022-04-19 16:28:54 +0300

    prom

--- gulpfile.js
+++ gulpfile.js
@@ -248,9 +248,9 @@ const htmlInclude = () => {
       prefix: '@',
       basepath: '@file'
     }))
-    .pipe(typograf({
-      locale: ['ru', 'en-US']
-    }))
+    // .pipe(typograf({
+    //   locale: ['ru', 'en-US']
+    // }))
     .pipe(dest(buildFolder))
     .pipe(browserSync.stream());
 }
